2e0e9831783c2dc53001e7c7aeea6905d02b995e
Adithyaaprogrammer <cadithyaa@gmail.com>
2024-08-28 22:54:18 +0530

    deleted

--- Code/extensions.py
+++
@@ -1,9 +0,0 @@
-from flask_restful import Api
-from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy.orm import declarative_base
-
-api = Api(prefix="/api/v1")
-
-Base = declarative_base()
-
-db = SQLAlchemy(model_class=Base)
